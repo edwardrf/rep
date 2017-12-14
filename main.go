177edb770f77cde6b1d43dd4d526e24ce453c061
Edward Jiang <edwardrf@gmail.com>
2017-12-14 16:16:45 +0800

    Minor changes to cut a few bytes

--- main.go
+++ main.go
@@ -15,30 +15,30 @@ import (
 
 func main() {
 	t := []byte(X)
-	p(t, os.Stdout, true)
+	p(t, os.Stdout, 1)
 }
-
-func p(t []byte, w io.Writer, o bool) {
+func p(t []byte, w io.Writer, o int) {
+	q := []byte{96}
 	for _, b := range t {
-		if o && b == 88 {
-			w.Write([]byte{96})
-			p(t, w, false)
-			w.Write([]byte{96})
+		if o > 0 && b == 88 {
+			w.Write(q)
+			p(t, w, 0)
+			w.Write(q)
 		} else {
 			w.Write([]byte{b})
 		}
 	}
 }
 `)
-	p(t, os.Stdout, true)
+	p(t, os.Stdout, 1)
 }
-
-func p(t []byte, w io.Writer, o bool) {
+func p(t []byte, w io.Writer, o int) {
+	q := []byte{96}
 	for _, b := range t {
-		if o && b == 88 {
-			w.Write([]byte{96})
-			p(t, w, false)
-			w.Write([]byte{96})
+		if o > 0 && b == 88 {
+			w.Write(q)
+			p(t, w, 0)
+			w.Write(q)
 		} else {
 			w.Write([]byte{b})
 		}
