d85ab7d929e8151a5afadd6cfa5b22e71b275e77
Edward Jiang <edwardrf@gmail.com>
2017-12-14 18:20:16 +0800

    Print itself using index version
    gofmt always removes the end space, so a place holder had to be placed

--- main.go
+++ main.go
@@ -1,48 +1,22 @@
 package main
 
 import (
-	"io"
 	"os"
 )
 
-type B []byte
-
 func main() {
-	t := B(`package main
+	t := `package main
 
 import (
-	"io"
 	"os"
 )
 
-type B []byte
-
 func main() {
-	t := B(X)
-	p(t, os.Stdout, 1)
+	t := _
+	q := "\u0060"
+	os.Stdout.WriteString(t[:52] + q + t + q + t[53:])
 }
-func p(t B, w io.Writer, o int) {
-	for _, b := range t {
-		if o > 0 && b == 88 {
-			w.Write(B{96})
-			p(t, w, 0)
-			w.Write(B{96})
-		} else {
-			w.Write(B{b})
-		}
-	}
-}
-`)
-	p(t, os.Stdout, 1)
-}
-func p(t B, w io.Writer, o int) {
-	for _, b := range t {
-		if o > 0 && b == 88 {
-			w.Write(B{96})
-			p(t, w, 0)
-			w.Write(B{96})
-		} else {
-			w.Write(B{b})
-		}
-	}
+`
+	q := "\u0060"
+	os.Stdout.WriteString(t[:52] + q + t + q + t[53:])
 }
