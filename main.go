54c66106dadd88b1952ab1066166d32905a4c518
Edward Jiang <edwardrf@gmail.com>
2017-12-14 16:28:56 +0800

    A bit more cutting

--- main.go
+++ main.go
@@ -5,42 +5,44 @@ import (
 	"os"
 )
 
+type B []byte
+
 func main() {
-	t := []byte(`package main
+	t := B(`package main
 
 import (
 	"io"
 	"os"
 )
 
+type B []byte
+
 func main() {
-	t := []byte(X)
+	t := B(X)
 	p(t, os.Stdout, 1)
 }
-func p(t []byte, w io.Writer, o int) {
-	q := []byte{96}
+func p(t B, w io.Writer, o int) {
 	for _, b := range t {
 		if o > 0 && b == 88 {
-			w.Write(q)
+			w.Write(B{96})
 			p(t, w, 0)
-			w.Write(q)
+			w.Write(B{96})
 		} else {
-			w.Write([]byte{b})
+			w.Write(B{b})
 		}
 	}
 }
 `)
 	p(t, os.Stdout, 1)
 }
-func p(t []byte, w io.Writer, o int) {
-	q := []byte{96}
+func p(t B, w io.Writer, o int) {
 	for _, b := range t {
 		if o > 0 && b == 88 {
-			w.Write(q)
+			w.Write(B{96})
 			p(t, w, 0)
-			w.Write(q)
+			w.Write(B{96})
 		} else {
-			w.Write([]byte{b})
+			w.Write(B{b})
 		}
 	}
 }
