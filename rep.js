2c623c7ff113280e7a6e5942afcc827b413029e3
Edward Jiang <edwardrf@gmail.com>
2017-12-14 22:46:01 +0800

    use es5 [] syntax to access string

--- rep.js
+++ rep.js
@@ -1,1 +1,1 @@
-var q=String.fromCharCode(34);var t="var q=String.fromCharCode(34);var t=;console.log(t.substr(0,36)+q+t+q+t.substr(36));";console.log(t.substr(0,36)+q+t+q+t.substr(36));
+var t="\\\"var t=;console.log(t.substr(2,6)+t[1]+t[0]+t[0]+t+t[1]+t.substr(8));";console.log(t.substr(2,6)+t[1]+t[0]+t[0]+t+t[1]+t.substr(8));
